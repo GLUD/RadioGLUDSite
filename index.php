05e19978836a576ce72233d11ea3f376d6bbc594
Jorge Useche <naturalmentejorge@gmail.com>
2016-05-25 21:52:42 -0500

    Se da la advertencia de no tener el módulo php_gettext

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php
+  if ( !function_exists('_') or !function_exists('gettext') ) {
+    function _($string) {
+    	return $string;
+	}
+	echo "<h1>Tal vez necesita installar gettext que está en la extensión php_gettext.</h1>";
+  }
   include("cod/header.html");
   include("cod/content.html");
   include("cod/footer.html");
